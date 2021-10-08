de795eeb746674cb2c1e7fadd2aa0608e4d19c16
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-10-08 09:44:39 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,11 +5,11 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1994325746:AAEdE8sDfHmwX9345yhJMrPVOdb5CoIir7I")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 6826285))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "916b4b01a784cf5c192e00ffacb3fc89")
 
     # Get this api from https://www.remove.bg/b/background-removal-api
-    RemoveBG_API = os.environ.get("RemoveBG_API", "")
+    RemoveBG_API = os.environ.get("RemoveBG_API", "o6fwphkpDUkREAg8NdAJCF2f")
